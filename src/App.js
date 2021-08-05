4a23c409afc5908f09039cf49fc43aad03958a8d
Alson33 <garbuja12alson@gmial.com>
2021-08-05 15:12:54 +0545

    add body to the web

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Container } from '@material-ui/core';
 import Header from './components/header/Header';
 import { useFetch } from './custom-hooks/UseFetch';
 import React, { useState, useEffect } from 'react';
+import Body from './components/body/Body';
 
 const url = "https://covid-193.p.rapidapi.com/countries"
 
 function App() {
   const { loading, responses } = useFetch(url)
   const [country, setCountry] = useState("Nepal")
+  
   const [countries, setCountries] = useState([])
   const [selectedDate, setSelectedDate] = useState(new Date('2021-08-04T12:11:54'))
 
@@ -17,20 +19,21 @@ function App() {
   }, [responses])
 
   return (
-    <div className="App">
-      {
-        loading || (
-          <Container maxWidth="md">
-            <Header 
-              country={country} 
-              setCountry={setCountry} 
-              countries={countries}
-              selectedDate={selectedDate}
-              handleDateChange={setSelectedDate}
-            />
-          </Container>
-        )
-      }
+    <div className="App"> 
+        {
+          loading || (
+            <Container maxWidth="md">
+              <Header 
+                country={country} 
+                setCountry={setCountry} 
+                countries={countries}
+                selectedDate={selectedDate}
+                handleDateChange={setSelectedDate}
+              />
+              <Body country={country}/>
+            </Container>
+          )
+        }
     </div>
   );
 }
