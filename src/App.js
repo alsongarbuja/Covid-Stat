990978141c8f138d014311e4efc0f0ecae38f109
Alson33 <garbuja12alson@gmial.com>
2021-08-10 22:33:59 +0545

    Add router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
 import './App.css';
-import { Container } from '@material-ui/core';
+import { Container, CircularProgress } from '@material-ui/core';
 import Header from './components/header/Header';
 import { useFetch } from './custom-hooks/UseFetch';
 import React, { useState, useEffect } from 'react';
 import Body from './components/body/Body';
+import MainBody from './components/body/MainBody';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const url = "https://covid-193.p.rapidapi.com/countries"
+const URL = 'https://covid-193.p.rapidapi.com/statistics'
 
 function App() {
-  const { loading, responses } = useFetch(url)
-  const [country, setCountry] = useState("Nepal")
-  
-  const [countries, setCountries] = useState([])
+  const { loading, responses } = useFetch(URL)
+
+  const [data, setData] = useState([])
   const [selectedDate, setSelectedDate] = useState(new Date('2021-08-04T12:11:54'))
 
   useEffect(() => {
-    setCountries(responses.response)
+      setData(responses.response)
   }, [responses])
+
+
 
   return (
     <div className="App"> 
-        {
-          loading || (
-            <Container maxWidth="md">
-              <Header 
-                country={country} 
-                setCountry={setCountry} 
-                countries={countries}
-                selectedDate={selectedDate}
-                handleDateChange={setSelectedDate}
-              />
-              <Body country={country}/>
-            </Container>
-          )
-        }
+      <Header
+        selectedDate={selectedDate}
+        handleDateChange={setSelectedDate}
+      />
+      {
+        loading ? (
+          <div className="progressDiv flex">
+            <CircularProgress />
+          </div>
+        ) : (
+          <>
+            <Router>
+              <Switch>
+                <Route exact path="/">
+                  <MainBody data={data} />
+                </Route>
+                <Route path="/detail/:country">
+                  <Container maxWidth="md">
+                    <Body />
+                  </Container>
+                </Route>
+              </Switch>
+            </Router>
+          </>
+        )
+      }
     </div>
   );
 }
