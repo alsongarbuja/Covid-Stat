3e66d361e7059bbe7888a6c0e6b1dbb354ff8c3f
Alson33 <garbuja12alson@gmial.com>
2021-08-24 01:18:41 +0545

    add new hook for NewsAPI

--- src/custom-hooks/UseFetch.js
+++ src/custom-hooks/UseFetch.js
@@ -8,7 +8,7 @@ export const useFetch = (url) => {
     const response = await fetch(url, {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "4b64913a5amsh811f53bd560bb6dp1aa97djsnddd46be6cbc0",
+            "x-rapidapi-key": process.env.REACT_APP_RAPID_API_COVID_KEY,
             "x-rapidapi-host": "covid-193.p.rapidapi.com"
         }
     });
@@ -22,3 +22,25 @@ export const useFetch = (url) => {
   }, [url, getResponses]);
   return { loading, responses };
 };
+
+export const useFetchApi = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [responses, setResponses] = useState([]);
+
+  const getResponses = useCallback(async () => {
+    const response = await fetch(url, {
+        "method": "GET",
+        "headers": {
+            "X-Api-Key": process.env.REACT_APP_NEWS_API_KEY
+        }
+    });
+    const responses = await response.json();
+    setResponses(responses);
+    setLoading(false);
+  }, [url]);
+
+  useEffect(() => {
+    getResponses();
+  }, [url, getResponses]);
+  return { loading, responses };
+}
