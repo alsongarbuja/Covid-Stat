671be631527a422c1de724ab49dff85476cd5e9f
Alson33 <garbuja12alson@gmial.com>
2021-08-24 21:36:36 +0545

    add a skeleton setting page

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Settings = () => {
+    return (
+        <div style={{ padding:"1em 0 0 2em" }}>
+            <h1>Settings</h1>
+        </div>
+    )
+}
+
+export default Settings
