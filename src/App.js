5bc3c6e761a8d158d4893223440150434fce46a0
Alson33 <garbuja12alson@gmial.com>
2021-08-24 21:35:05 +0545

    add new routes and components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
-import { Container, CircularProgress } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import Header from './components/header/Header';
 import { useFetch } from './custom-hooks/UseFetch';
 import React, { useState, useEffect } from 'react';
 import Body from './components/body/Body';
 import MainBody from './components/body/MainBody';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Dashboard from './components/body/Dashboard/Dashbord';
+import Sidebar from './components/sidebar/Sidebar';
+import Myths from './components/Myths/Myths';
+import Settings from './components/Settings/Settings';
+
+import { Toolbar, Wrapper } from './App.style'
+import { useMediaQuery } from '@material-ui/core';
 
 const URL = 'https://covid-193.p.rapidapi.com/statistics'
 
 function App() {
   const { loading, responses } = useFetch(URL)
+  const isMobile = useMediaQuery('(max-width: 600px)')
 
   const [data, setData] = useState([])
-  const [selectedDate, setSelectedDate] = useState(new Date('2021-08-04T12:11:54'))
   const [searchedCountry, setSearchedCountry] = useState('')
 
   useEffect(() => {
@@ -23,37 +30,40 @@ function App() {
 
   return (
     <Router>
-      <div className="App"> 
-        {
-          window.location.pathname !== '/' ? (
-            <Header
-              selectedDate={selectedDate}
-              handleDateChange={setSelectedDate}
-              setSearchedCountry={setSearchedCountry}
-            />
-          ) : (<></>)
-        }
-        {
-          loading ? (
-            <div className="progressDiv flex">
-              <CircularProgress />
-            </div>
-          ) : (
-            <Switch>
-              <Route exact path="/">
-                <Dashboard />
-              </Route>
-              <Route exact path="/dashboard">
-                <MainBody data={data} searchedCountry={searchedCountry} />
-              </Route>
-              <Route path="/detail/:country">
-                <Container maxWidth="md">
-                  <Body />
-                </Container>
-              </Route>
-            </Switch>
-          )
-        }
+      <div style={{ display:"flex", height:"100%", width:"100%" }}> 
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Wrapper>
+              <Dashboard />
+              {isMobile ? <Toolbar /> : <></>}
+            </Wrapper>
+          </Route>
+          <Route exact path="/stats">
+            <Wrapper>
+              <Header setSearchedCountry={setSearchedCountry} />
+              <MainBody loading={loading} data={data} searchedCountry={searchedCountry} />
+              {isMobile ? <Toolbar /> : <></>}
+            </Wrapper>
+          </Route>
+          <Route path="/detail/:country">
+            <Container maxWidth="md">
+              <Body />
+            </Container>
+          </Route>
+          <Route exact path="/myths">
+            <Wrapper>
+              <Myths />
+              {isMobile ? <Toolbar /> : <></>}
+            </Wrapper>
+          </Route>
+          <Route>
+            <Wrapper>
+              <Settings />
+              {isMobile ? <Toolbar /> : <></>}
+            </Wrapper>
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
