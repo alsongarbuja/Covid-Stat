77f11d80cf2d7a5c375e11dd2d0d452d1eba20cc
Alson33 <garbuja12alson@gmial.com>
2021-08-29 16:16:42 +0545

    add dynamic country fetch for dashboard

--- src/components/body/Dashboard/Dashbord.jsx
+++ src/components/body/Dashboard/Dashbord.jsx
@@ -2,7 +2,7 @@ import React , { useState, useEffect } from 'react'
 
 import { InfoHolder, ReportDiv, SymptomDiv } from './Dashboard.style'
 
-import { Grid, useMediaQuery } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import { ArrowForward, ScatterPlot } from '@material-ui/icons'
 import { Link } from 'react-router-dom'
 
@@ -11,9 +11,11 @@ import Chart from './Report/Report'
 
 import { useFetch } from '../../../custom-hooks/UseFetch'
 
-const Dashbord = ({ isDarkMode, language }) => {
-    const isMobile = useMediaQuery('(max-width: 600px)')
-    const { loading, responses } = useFetch('https://covid-193.p.rapidapi.com/statistics?country=Nepal', "covid-193.p.rapidapi.com")
+const Dashbord = ({ isMobile, isDarkMode, language }) => {
+    const selectedCountry = localStorage.getItem('country') === null ? 'Nepal' : localStorage.getItem('country')
+    const URL = `https://covid-193.p.rapidapi.com/statistics?country=${selectedCountry}`
+    
+    const { loading, responses } = useFetch(URL, "covid-193.p.rapidapi.com")
 
     const [data, setData] = useState([])
 
@@ -24,11 +26,11 @@ const Dashbord = ({ isDarkMode, language }) => {
     return (
         <Grid container style={{ paddingTop:"1em", paddingLeft: isMobile ? "0":"2em" }}>
             <Grid item xl={6} md={9} xs={12}>
-                <h1 style={{ paddingLeft:".75em" }}>DashBoard({loading ? "" : data[0].country})</h1>
+                <h1 style={{ paddingLeft:".75em" }}>DashBoard({loading ? "-" : data[0].country})</h1>
                 <div style={{ display:"flex", flexWrap:"wrap", marginBottom:"1em"}}>
                     <InfoHolder isDarkMode={isDarkMode} style={{ backgroundColor:"#e6600ecc" }}>
                         <h3>New cases</h3>
-                        <p>{loading ? "-" : data[0].cases.new}</p>
+                        <p>{loading ? "-" : data[0].cases.new ? data[0].cases.new : "0"}</p>
                     </InfoHolder>
                     <InfoHolder isDarkMode={isDarkMode} style={{ backgroundColor:"#241de5c0" }}>
                         <h3>Tested</h3>
@@ -36,7 +38,7 @@ const Dashbord = ({ isDarkMode, language }) => {
                     </InfoHolder> 
                     <InfoHolder isDarkMode={isDarkMode} style={{ backgroundColor:"#fb2528d8" }}>
                         <h3>New Deaths</h3>
-                        <p>{loading ? "-" : data[0].deaths.new}</p>
+                        <p>{loading ? "-" : data[0].deaths.new ? data[0].deaths.new : "0"}</p>
                     </InfoHolder>
                     <InfoHolder isDarkMode={isDarkMode} style={{ backgroundColor:"#60bd08fa" }}>
                         <h3>Recovered</h3>
